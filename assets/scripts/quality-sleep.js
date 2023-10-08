d7d09c9bb9b4366bef38f26491bbd5177e617c46
MyselfRoshan <roshanghimire699@gmail.com>
2023-10-08 23:28:00 +0545

    feat: completed storage of stay hydrated and daily exercise to local storage

---
+++ assets/scripts/quality-sleep.js
@@ -0,0 +1,21 @@
+// // script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   const saveButton = document.getElementById("save-button");
+//   const startTimeInput = document.getElementById("start-time");
+//   const endTimeInput = document.getElementById("end-time");
+
+//   // const options = { okLabel: "test", amLabel: "test1", backdrop: false };
+//   // const initTimepicker = new TimepickerUI(startTimeInput, options);
+
+//   // timepicker.create();
+//   saveButton.addEventListener("click", function () {
+//     const startTime = startTimeInput.value;
+//     const endTime = endTimeInput.value;
+
+//     alert(`Sleep Start Time: ${startTime}\nSleep End Time: ${endTime}`);
+//   });
+//   const timepicker = document.querySelector("timepicker-ui-test");
+//   const init = new TimepickerUI(timepicker);
+
+//   timepicker.create();
+// });
--- assets/scripts/stay-hydrated.js
+++ assets/scripts/stay-hydrated.js
@@ -1,96 +1,143 @@
 const glassWater = document.querySelector(".glass-water");
+const glassWaterForm = document.querySelector(".glass-water__form");
 const glassAdd = document.querySelector(".glass-add");
 const glassRemove = document.querySelector(".glass-remove");
 const setGlassTarget = document.querySelector("#set-glass-target");
 const glassTarget = document.querySelector(".glass-target");
 const glassToIntake = document.querySelector(".glass-to-intake");
 const waterToIntake = document.querySelector(".water-to-intake");
+const selectDate = document.querySelector("#select-date");
 
 const maximumGlassTarget = parseInt(setGlassTarget.getAttribute("max"));
 const minimumGlassTarget = parseInt(setGlassTarget.getAttribute("min"));
-const waterPerGlass = 250; //250ml
+const waterPerGlass = 250; // 250ml
 
-setGlassTarget.addEventListener("input", e => {
-  let inputValue = e.target.value;
-  // Only allow numbers [0-9]
-  inputValue = inputValue.replace(/\D/g, "");
-  // Ensure the value is within the specified range
-  if (inputValue < 1) inputValue = 1;
-  else if (inputValue > 50) inputValue = 50;
-  e.target.value = inputValue;
+let Water = JSON.parse(localStorage.getItem("Water")) || {};
 
-  if (Number(inputValue) == 1) glassTarget.textContent = `${inputValue} Glass`;
-  else glassTarget.textContent = `${inputValue} Glasses`;
+function updateWaterData(date) {
+  if (date in Water) {
+    setGlassTarget.value = Water[date].target;
+    glassToIntake.textContent = Water[date].intaked;
+    glassTarget.textContent = `${setGlassTarget.value} Glass${
+      setGlassTarget.value != "1" ? "es" : ""
+    }`;
+    waterToIntake.textContent = `(${
+      parseInt(glassToIntake.textContent) * waterPerGlass
+    } ml)`;
+    waterLevel("new-target");
+  }
+}
 
-  waterLevel("new-target");
-});
-setGlassTarget.addEventListener("change", () => {});
+function updateLocalStorage() {
+  Water[selectDate.value] = {
+    target: parseInt(setGlassTarget.value),
+    intaked: parseInt(glassToIntake.textContent),
+  };
+  localStorage.setItem("Water", JSON.stringify(Water));
+}
 
-document.addEventListener("DOMContentLoaded", () => {
+function handleGlassAction(increase) {
   let glassToIntakeValue = Number(glassToIntake.textContent);
-  if (glassToIntakeValue == minimumGlassTarget - 1)
+
+  if (increase) {
+    glassToIntake.textContent = glassToIntakeValue + 1;
+  } else {
+    glassToIntake.textContent = glassToIntakeValue - 1;
+  }
+
+  waterToIntake.textContent = `(${
+    parseInt(glassToIntake.textContent) * waterPerGlass
+  } ml)`;
+  waterLevel(increase ? "increase" : "decrease");
+
+  if (increase) {
+    if (glassToIntakeValue < maximumGlassTarget) {
+      glassRemove.removeAttribute("disabled");
+    }
+
+    if (glassToIntakeValue === maximumGlassTarget - 1) {
+      glassAdd.setAttribute("disabled", "true");
+    }
+  } else {
+    if (glassToIntakeValue > minimumGlassTarget) {
+      glassAdd.removeAttribute("disabled");
+    }
+
+    if (glassToIntakeValue === minimumGlassTarget) {
+      glassRemove.setAttribute("disabled", "true");
+    }
+  }
+
+  updateLocalStorage();
+}
+
+function handleSetGlassTargetChange(e) {
+  let inputValue = e.target.value;
+  inputValue = inputValue.replace(/\D/g, "");
+  inputValue = Math.min(50, Math.max(1, inputValue));
+  e.target.value = inputValue;
+  glassTarget.textContent = `${inputValue} Glass${
+    inputValue !== "1" ? "es" : ""
+  }`;
+  glassTarget.textContent = `${setGlassTarget.value} Glass${
+    parseInt(setGlassTarget.value) !== 1 ? "es" : ""
+  }`;
+  waterLevel("new-target");
+  updateLocalStorage();
+}
+
+function handleDateChange() {
+  updateWaterData(selectDate.value);
+  updateLocalStorage();
+}
+
+// Initialize the UI
+document.addEventListener("DOMContentLoaded", () => {
+  const currentDate = selectDate.value;
+  updateWaterData(currentDate);
+
+  let glassToIntakeValue = Number(glassToIntake.textContent);
+  if (glassToIntakeValue === minimumGlassTarget - 1)
     glassRemove.setAttribute("disabled", "true");
-  if (glassToIntakeValue == maximumGlassTarget - 1)
+  if (glassToIntakeValue === maximumGlassTarget - 1)
     glassAdd.setAttribute("disabled", "true");
-  // waterLevel("new-target");
+
+  setGlassTarget.addEventListener("input", handleSetGlassTargetChange);
 
   const inputTxtFields = document.querySelectorAll(".input-signup");
-  console.log(inputTxtFields);
   inputTxtFields.forEach(inputTxtField => {
-    // inputTxtField.
     inputTxtField.addEventListener("input", () => {
       inputTxtField.setAttribute("value", inputTxtField.value);
       localStorage.setItem(`${inputTxtField.id}`, inputTxtField.value);
     });
   });
+
+  selectDate.value = NepaliFunctions.GetCurrentBsDate("YYYY-MM-DD");
+  selectDate.nepaliDatePicker({
+    language: "english",
+    dateFormat: "YYYY-MM-DD",
+    ndpYear: true,
+    ndpMonth: true,
+    ndpYearCount: 10,
+    readOnlyInput: true,
+    disableDaysBefore: 365,
+    disableDaysAfter: 0,
+    onChange: handleDateChange,
+  });
 });
 
-glassAdd.addEventListener("click", () => {
-  let glassToIntakeValue = Number(glassToIntake.textContent);
-  glassToIntake.textContent = glassToIntakeValue + 1;
-  waterToIntake.textContent = `(${
-    parseInt(glassToIntake.textContent) * waterPerGlass
-  } ml)`;
-  waterLevel("increase");
-
-  if (glassToIntakeValue < maximumGlassTarget)
-    glassRemove.removeAttribute("disabled");
-
-  if (glassToIntakeValue == maximumGlassTarget - 1)
-    glassAdd.setAttribute("disabled", "true");
-});
-
-glassRemove.addEventListener("click", () => {
-  let glassToIntakeValue = Number(glassToIntake.textContent);
-  glassToIntake.textContent = glassToIntakeValue - 1;
-  waterToIntake.textContent = `(${
-    parseInt(glassToIntake.textContent) * waterPerGlass
-  } ml)`;
-  waterLevel("decrease");
-
-  if (glassToIntakeValue > minimumGlassTarget)
-    glassAdd.removeAttribute("disabled");
-
-  if (glassToIntakeValue == minimumGlassTarget)
-    glassRemove.setAttribute("disabled", "true");
-});
+glassAdd.addEventListener("click", () => handleGlassAction(true));
+glassRemove.addEventListener("click", () => handleGlassAction(false));
 
 function waterLevel(controller) {
-  // Water level controller
-  // Here -94% and -200% is top property of ::before and ::after pseudo element of water when empty and full
   let noOfGlassTarget = parseInt(setGlassTarget.value);
   let noOfGlassToIntake = parseInt(glassToIntake.textContent);
-
-  // Calculate the percentage change
   let percentageChange = 106 / noOfGlassTarget;
-
-  // Get the current wave height
   let waveHeight = parseFloat(
     getComputedStyle(glassWater).getPropertyValue("--wave"),
   );
-
-  // Calculate the new wave height based on the controller
   let newHeight = waveHeight;
+
   if (controller === "increase" && noOfGlassToIntake <= noOfGlassTarget) {
     newHeight -= percentageChange;
   } else if (controller === "decrease" && noOfGlassTarget > noOfGlassToIntake) {
@@ -99,9 +146,6 @@ function waterLevel(controller) {
     newHeight = -((noOfGlassToIntake / noOfGlassTarget) * 106 + 100);
   }
 
-  // Ensure the new height is within the bounds
   newHeight = Math.max(-206, Math.min(-100, newHeight));
-
-  // Update the wave height
   glassWater.style.setProperty("--wave", `${newHeight}%`);
 }
